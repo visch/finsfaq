bd838b2b7132cd5d8575f649a64644c8cfc27202
Charing Wong <kozzi@users.noreply.github.com>
2016-06-06 17:18:14 -0400

    Update common.js
    Enable hover tooltips with Bootstrap

--- js/common.js
+++ js/common.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
+  var footnotesElements = $("li[id^='fn:fn']");
+
+  footnotesElements.each( function (index) {
+    // Get the footnote content
+    var footnoteElement = $(this).clone();
+    footnoteElement.children("p").children("a.reversefootnote").remove();
+    var footnoteHTML = footnoteElement.children("p").html();    
+    
+    // Get the footnote id
+    var footnoteId = footnoteElement.attr("id");
+    
+    // Find the referencing link
+    var refLink = $("a[href$='" + footnoteId + "']");
+    
+    // Set the data-toggle attributes
+    var attributes = {
+      "data-toggle": "tooltip",
+      "title": footnoteHTML
+    };
+    refLink.attr(attributes);
+    
+    // Initialize tooltips with options
+    var tooltipOptions = {
+      "html": true,
+      "placement": "auto",
+      "container": refLink,
+      "delay": {hide:200}
+    };
+    refLink.tooltip(tooltipOptions);
   });
+});
